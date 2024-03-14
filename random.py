6b7cef14affcd0dcc8eeee78d8d06ce3fa1ecfe0
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 16:15:05 +0700

    Update random.py

--- random.py
+++ random.py
@@ -11,6 +11,6 @@ class EveryMod(loader.Module):
     """Every модуль"""
     strings = {'name': 'Every'}
 
-async def watcher(self, message: Message):
+async def watcher(self, message:Message):
     if message.text.lower() == 'HI':
         await utils.answer(message, '/leave')
