ab70d40ddd6bc381fbf30e37873506586f377284
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 16:30:32 +0700

    Update random.py

--- random.py
+++ random.py
@@ -1,19 +1,25 @@
-import datetime
-import logging
-import time
-import re
-from telethon.tl.types import Message
-
-from telethon import types
-
 from .. import loader, utils
+from telethon.tl.types import Message
+import re
+import logging
+logger = logging.getLogger(__name__)
 
-class EveryMod(loader.Module):
-    """Every модуль"""
-    strings = {'name': 'Every'}
 
-async def watcher(self, message:Message):
-    a = 'HI'
-    if message.text = a:
-        await utils.answer(message, '/leave')
+@loader.tds
+class Mod(loader.Module):
+    strings = {
+        'name': 'ModForDan',
+
+        "get_user": "🚀Пользователь: \n"
+        "<b>🥷🏻</b> <a href='tg://openmessage?user_id={0}'>{1}</a> \n"
+        "<b>📃Юзернейм:</b> @{2} \n"
+        "<b>📋Юзер</b> : #user{0} \n"
+        "📌<b>Бан</b> : <code>/banid {0}</code> \n"
+        "<b>🆔Айди : </b> <code>@{3}</code>",
+    }
+
+    async def watcher(self, message: Message):
+    while True:
+        async with message.client.conversation("username") as conv:
+            conv.send_message("text")
     
