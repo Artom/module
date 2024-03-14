4a0a04bd63b93652ac7bcc461a38c1b67c776315
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 16:01:44 +0700

    Update random.py

--- random.py
+++ random.py
@@ -1,28 +1,15 @@
+import datetime
+import logging
+import time
+
+from telethon import types
+
 from .. import loader, utils
-from asyncio import sleep, gather
-from telethon import TelegramClient, events
-from telethon.sync import TelegramClient
-from telethon.tl import functions, types
-from telethon import utils
 
 class EveryMod(loader.Module):
     """Every модуль"""
     strings = {'name': 'Every'}
 
-async def main():
-        @client.on(events.NewMessage(pattern=r'.*\b(?:спонс|Спонс|СПОНС|spons|Spons|спонса|Спонса|Спонсы|спонсы|sponsi|cgjyc|Cgjyc|cgjycf|Cgjycf)\b.*'))
-        async def handle_sponsor_message(event):
-            if event.is_private:
-                await event.respond('Привет, спонсы не выдаю!')
-
-        @client.on(events.NewMessage(pattern=r'.*\b(?:пошел нахуй|Пошел нахуй)\b.*'))
-        async def handle_sponsor_message(event):
-            if event.is_private:
-                await event.respond('Без осков пж')   
-
-        await client.run_until_disconnected()
-
-
-if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
+async def watcher(self, message):
+    if str(message.text) == 'HI':
+        await utils.answer(message, '/leave')
