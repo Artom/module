2a2ece89a5712499c4d1b1797391ed87bdcc1979
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 14:57:38 +0700

    Update random.py

--- random.py
+++ random.py
@@ -6,10 +6,26 @@ def register(cb):
     cb(SpamMod())
 
 class SpamMod(loader.Module):
-    """Everyvмодуль"""
-    strings = {'name': 'Every'}
+    """Спам модуль"""
+    strings = {'name': 'Spam'}
 
-    async def stcmd(self, message):
+    async def spamcmd(self, message):
         """Обычный спам. Используй .spam <кол-во:int> <текст или реплай>."""
         try:
-            await message.client.send_message(message.to_id, '/start')
+            await message.delete()
+            args = utils.get_args(message)
+            count = int(args[0].strip())
+            reply = await message.get_reply_message()
+            if reply:
+                if reply.media:
+                    for _ in range(count):
+                        await message.client.send_file(message.to_id, reply.media)
+                    return
+                else:
+                    for _ in range(count):
+                        await message.client.send_message(message.to_id, reply)
+            else:
+                message.message = " ".join(args[1:])
+                for _ in range(count):
+                    await gather(*[message.respond(message)])
+        except: return await message.client.send_message(message.to_id, '.spam <кол-во:int> <текст или реплай>.')
