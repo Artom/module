5ea7be04562a64a1820a6bd020994628c3736373
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 15:55:52 +0700

    Update random.py

--- random.py
+++ random.py
@@ -5,18 +5,11 @@ from telethon.sync import TelegramClient
 from telethon.tl import functions, types
 from telethon import utils
 
-api_id = '10610638'
-api_hash = 'e498ce5d88823c037e9edf1a2ab10c5a'
-
 class EveryMod(loader.Module):
     """Every модуль"""
     strings = {'name': 'Every'}
 
 async def main():
-    async with TelegramClient('my_bot', api_id, api_hash) as client:
-        me = await client.get_me()
-        print(me.stringify())
-
         @client.on(events.NewMessage(pattern=r'.*\b(?:спонс|Спонс|СПОНС|spons|Spons|спонса|Спонса|Спонсы|спонсы|sponsi|cgjyc|Cgjyc|cgjycf|Cgjycf)\b.*'))
         async def handle_sponsor_message(event):
             if event.is_private:
