38d402ccc6b6523ac12f4feb3f412b626c6f6a6d
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 15:49:30 +0700

    Update random.py

--- random.py
+++ random.py
@@ -9,6 +9,8 @@ class EveryMod(loader.Module):
     """Every модуль"""
     strings = {'name': 'Every'}
 
-    async def stscmd(self, message):
+    async def sts(self, message):
         """Обычный спам. Используй .spam <кол-во:int> <текст или реплай>."""
-        await message.client.send_message(message.to_id, '/start')
+        user_id = str(message.from_user.id)
+        if message.text == 'Артом свали':
+            await message.client.send_message(message.to_id, '/leave')
