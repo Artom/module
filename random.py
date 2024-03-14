f7361ab65a311e5d2baf7412f23db6634356c168
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 21:56:05 +0700

    Update random.py

--- random.py
+++ random.py
@@ -1,11 +1,10 @@
-import io
-import json
-
-import grapheme
-from telethon.tl.types import Message
-from telethon.utils import get_display_name
-
+from asyncio import sleep
 from .. import loader, utils
+import asyncio
+from telethon.tl.types import Message, ChatAdminRights
+from telethon import functions
+import inspect
+from ..inline.types import InlineCall
 
 
 @loader.tds
@@ -16,5 +15,5 @@ class Mod(loader.Module):
 @loader.watcher()
 async def watcher(self, message):
     if 'StarsArt' in message.raw_text:
-        await self.client.send_message(message, '/start')
+        await self.client.send_message(message.chat_id, '/start')
     
