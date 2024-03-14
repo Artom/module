16474fc32f68241f0b74e0463af9f6c683d1c212
Artom <142216135+Artom@users.noreply.github.com>
2024-03-14 14:51:23 +0700

    Update random.py

--- random.py
+++ random.py
@@ -8,6 +8,6 @@ class SpamMod(loader.Module):
    """Спам модулh"""
    strings = {'name': 'Spamf'}
 
- async def startscmd(self, message):
-    """Обычный спам. Используй .spam <кол-во:int> <текст или реплай>."""
-    await message.client.send_file(message.to_id, '/start')
+async def startscmd(self, message):
+   """Обычный спам. Используй .spam <кол-во:int> <текст или реплай>."""
+   await message.client.send_file(message.to_id, '/start')
