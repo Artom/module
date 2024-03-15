7f6beaa1fea414b933ac6e73247256b8b82773eb
Artom <142216135+Artom@users.noreply.github.com>
2024-03-15 11:16:18 +0700

    Update random.py

--- random.py
+++ random.py
@@ -7,10 +7,15 @@ class EveryMod(loader.Module):
     @loader.owner
     async def startcmd(self, message):
         """используй <code>start</code> для начала"""
-        await utils.answer(message, '/start')
+        await utils.answer(message, '/start@EveryFloodilkaBot')
 
     @loader.owner
     async def leavecmd(self, message):
         """используй leave для выхода"""
-        await utils.answer(message, '/leave')
+        await utils.answer(message, '/leave@EveryFloodilkaBot')
+
     
+    @loader.owner
+    async def call1cmd(self, message):
+        """используй leave для выхода"""
+        await message[0].click(0)
