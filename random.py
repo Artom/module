797b607cf62c250f4f0b49d1bb7c279167e63a7f
Artom <142216135+Artom@users.noreply.github.com>
2024-03-15 11:06:33 +0700

    Update random.py

--- random.py
+++ random.py
@@ -1,11 +1,16 @@
 from .. import loader, utils
 
 @loader.tds
-class EverysMod(loader.Module):
-    strings = {"name": "Everyyy"}
+class EveryMod(loader.Module):
+    strings = {"name": "Every"}
 
     @loader.owner
     async def startcmd(self, message):
         """используй <code>start</code> для начала"""
         await utils.answer(message, '/start')
+
+    @loader.owner
+    async def leavecmd(self, message):
+        """используй leave для выхода"""
+        await utils.answer(message, '/leave')
     
