928399a90e0da56e49622af02c6aa663eb4112bd
Artom <142216135+Artom@users.noreply.github.com>
2024-03-15 10:29:36 +0700

    Update random.py

--- random.py
+++ random.py
@@ -1,16 +1,12 @@
-import hikka.utils.http as http
-import hikka.utils.helpers as helpers
-from .. import loader, utils
+from .. import loader
+from asyncio import sleep
+
 
 @loader.tds
-class AFKMod(loader.Module):
-    """Every"""
+class EveryMod(loader.Module):
+    strings = {"name": "Every"}
 
-    strings = {
-        "name": "Every"}
-
-@hikka.on(hikka.cmd(pattern='prem'))
-async def prem(event):
-    async with event.client.conversation(event.chat_id) as conv:
-        await conv.send_message('/leave')
+    @loader.owner
+    async def startcmd(self, message):
+        await message.send_message('/start')
     
